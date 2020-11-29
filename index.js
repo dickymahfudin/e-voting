584f52edec4058eb32c25831380cab2ac2efe5a7
dickymahfudin <dickymahfudin13@gmail.com>
2020-11-29 18:28:04 +0700

    route add view

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const apiRoute = require("./api");
@@ -6,15 +7,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app.use(bodyParser.json({ limit: "100mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
 app.listen(PORT, () =>
   console.log(`Server Running on : http://localhost:${PORT}`)
 );
 
 app.get("/", (req, res) => {
-  res.send("dism");
+  res.render("index", { data: 2 });
 });
 
 app.use("/api", apiRoute);
